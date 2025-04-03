08937599d15cf47f121314d4db133a4c8f92a6f5
AngelG01 <50680447+AngelG01@users.noreply.github.com>
2025-04-03 13:36:43 +0000

    solve 1 and 2 task

--- 01. Sections/sections.js
+++ 01. Sections/sections.js
@@ -2,4 +2,23 @@ document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
    // TODO
+   let form = document.getElementById('task-input');
+   let divContainer = document.getElementById('content');
+
+   form.addEventListener('submit', function(event){
+      event.preventDefault();
+
+      let inputField = form.querySelector('input[type="text"]').value;
+      let sections = inputField.split(',');
+
+      for (let section of sections){
+         let divEl = document.createElement('div');
+         let paragraph = document.createElement('p');
+
+         paragraph.textContent = section;
+         divEl.appendChild(paragraph)
+         divContainer.appendChild(divEl);
+      }
+   })
+   
 }
