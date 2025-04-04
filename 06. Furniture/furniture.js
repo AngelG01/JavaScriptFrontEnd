94716fd9647d76f38585ab88f373a6f58baa97c0
AngelG01 <50680447+AngelG01@users.noreply.github.com>
2025-04-04 13:57:42 +0000

    solve tasks

--- 06. Furniture/furniture.js
+++ 06. Furniture/furniture.js
@@ -2,4 +2,18 @@ document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
   //TODO
+  let form = document.getElementById('input');
+
+  form.addEventListener('submit', function(event) {
+      event.preventDefault();
+
+      let textArea = form.querySelector('textarea').value;
+      let convertedArray = JSON.parse(textArea)
+
+      let table = document.getElementById
+      for (element of convertedArray){
+          pass
+      }
+
+  })
 }
