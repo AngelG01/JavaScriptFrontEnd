22bd0017c9c51c0cf2d92fc446670e7fcb46eac4
AngelG01 <50680447+AngelG01@users.noreply.github.com>
2025-04-04 12:58:18 +0000

    fill-dropdown task

--- 04. Fill-Dropdown/fill-dropdown.js
+++ 04. Fill-Dropdown/fill-dropdown.js
@@ -2,4 +2,31 @@ document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
     //TODO
+    let submitButton = document.querySelector('input[type="submit"]');
+    let menu = document.getElementById('menu')
+
+    
+
+    submitButton.addEventListener('click', function(event){
+        event.preventDefault();
+        
+        let text = document.getElementById('newItemText').value;
+        let key = document.getElementById('newItemValue').value;
+
+
+        if (key && text){
+            let option = document.createElement('option')
+            option.value = key
+            option.text = text
+
+            menu.appendChild(option) 
+        
+            document.getElementById('newItemText').value = '';
+            document.getElementById('newItemValue').value = ''
+        }
+        
+    })
+
+
+
 }
